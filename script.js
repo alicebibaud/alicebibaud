32fa0ac1842ae9f3900f648aa1af6f9ccd727ecf
alicebibaud <alice.bibaud@gmail.com>
2023-03-31 16:50:55 -0400

    now this is one beefy repo

--- script.js
+++ script.js
@@ -1,4 +1,73 @@
-var panel = document.getElementById('BOLSTER');
-var overlay = document.createElement('div');
-overlay.id = 'overlay';
-panel.appendChild(overlay);
+function columnFunction1(imgs) {
+  // Get the image text
+  let img = document.getElementById("img1");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+} 
+
+
+function columnFunction2(imgs) {
+  // Get the image text
+  let img = document.getElementById("img2");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+} 
+
+
+function columnFunction3(imgs) {
+  // Get the image text
+  let img = document.getElementById("img3");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+} 
+
+function openAlb1(imgs) {
+  // Get the image text
+  let img = document.getElementById("album1");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+} 
+
+function openAlb2(imgs) {
+  // Get the image text
+  let img = document.getElementById("album2");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+}
+
+function openAlb3(imgs) {
+  // Get the image text
+  let img = document.getElementById("album3");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+}
+
+function openAlb4(imgs) {
+  // Get the image text
+  let img = document.getElementById("album4");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+}
+
+function openAlb5(imgs) {
+  // Get the image text
+  let img = document.getElementById("album5");
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  img.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  img.parentElement.style.display = "block";
+}
